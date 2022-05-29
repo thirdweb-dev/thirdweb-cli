27d8fec9dc54a54d9011f7fe6faad594ecf0238e
Joaquim Verges <joaquim.verges@gmail.com>
2022-05-29 16:49:09 -0700

    Re-enable foundry with metadata workaround

--- src/core/builder/foundry.ts
+++ src/core/builder/foundry.ts
@@ -1,4 +1,4 @@
-import { spinner } from "../helpers/logger";
+import { logger, spinner } from "../helpers/logger";
 import { CompileOptions } from "../interfaces/Builder";
 import { ContractPayload } from "../interfaces/ContractPayload";
 import { BaseBuilder } from "./builder-base";
@@ -36,11 +36,40 @@ export class FoundryBuilder extends BaseBuilder {
       const contractJsonFile = readFileSync(file, "utf-8");
 
       const contractInfo = JSON.parse(contractJsonFile);
+
+      if (
+        !contractInfo.bytecode ||
+        !contractInfo.deployedBytecode ||
+        !contractInfo.metadata
+      ) {
+        logger.debug("Skipping", contractName, "(no bytecode or metadata)");
+        continue;
+      }
+
       const bytecode = contractInfo.bytecode.object;
+      const deployedBytecode = contractInfo.deployedBytecode.object;
 
-      const metadata = JSON.stringify(contractInfo.metadata);
+      // Grab the raw metadata
+      const rawMeta = contractInfo.metadata;
+      rawMeta.output.abi = contractInfo.abi;
+      // need to re-add libraries if not present since forge stripts it out
+      if (!rawMeta.settings.libraries) {
+        rawMeta.settings.libraries = {};
+      }
+      // delete `outputSelection` from the metadata which has nothing to do here, bug in forge
+      delete rawMeta.settings.outputSelection;
+      // sort the metadata ALPHABETICALLY, since forge shuffles the keys in their parsing
+      const meta: any = this.sort(rawMeta);
+      // finally, here's the actual solc output that we expect
+      const metadata = JSON.stringify(meta);
 
-      if (this.shouldProcessContract(bytecode, contractName)) {
+      if (
+        this.shouldProcessContract(
+          contractInfo.abi,
+          deployedBytecode,
+          contractName,
+        )
+      ) {
         contracts.push({
           name: contractName,
           metadata,
@@ -50,5 +79,18 @@ export class FoundryBuilder extends BaseBuilder {
     }
     loader.succeed("Compilation successful");
     return { contracts };
+  }
+
+  private sort(object: any) {
+    if (typeof object != "object" || object instanceof Array)
+      // Not to sort the array
+      return object;
+    var keys = Object.keys(object);
+    keys.sort();
+    var newObject: any = {};
+    for (var i = 0; i < keys.length; i++) {
+      newObject[keys[i]] = this.sort(object[keys[i]]);
+    }
+    return newObject;
   }
 }
