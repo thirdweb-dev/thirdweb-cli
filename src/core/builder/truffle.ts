fbc39f136c34c7dd2d9a4770a5a54b0b92564b88
Joaquim Verges <joaquim.verges@gmail.com>
2022-06-09 15:33:13 -0700

    Extract sources and upload to IPFS in batches (#20)
    * extract sources from hardhat/foundry/solc
    
    * extract sources for truffle
    
    * remove logs
    
    * expand simple drop example

--- src/core/builder/truffle.ts
+++ src/core/builder/truffle.ts
@@ -49,11 +49,31 @@ export class TruffleBuilder extends BaseBuilder {
         continue;
       }
 
+      const meta = JSON.parse(metadata);
+      const sources = Object.keys(meta.sources)
+        .map((path) => {
+          path = path.replace("project:/", "");
+          const sourcePath = join(options.projectPath, path);
+          if (existsSync(sourcePath)) {
+            return sourcePath;
+          }
+          const nodeModulesPath = join(
+            options.projectPath,
+            "node_modules",
+            path,
+          );
+          if (existsSync(nodeModulesPath)) {
+            return nodeModulesPath;
+          }
+          return undefined;
+        })
+        .filter((path) => path !== undefined) as string[];
       if (this.shouldProcessContract(abi, deployedBytecode, contractName)) {
         contracts.push({
           metadata,
           bytecode,
           name: contractName,
+          sources,
         });
       }
     }
