ea9dfda3c135421444b9e41b2fdd479525c66786
Joaquim Verges <joaquim.verges@gmail.com>
2022-06-09 15:30:50 -0700

    expand simple drop example

--- examples/foundry/contracts/SimpleDrop.sol
+++ examples/foundry/contracts/SimpleDrop.sol
@@ -7,8 +7,9 @@ import "@thirdweb-dev/contracts/feature/ContractMetadata.sol";
 import "@thirdweb-dev/contracts/feature/PermissionsEnumerable.sol";
 import "@thirdweb-dev/contracts/feature/Royalty.sol";
 import "@thirdweb-dev/contracts/feature/Multicall.sol";
+import "@thirdweb-dev/contracts/feature/Drop.sol";
 
-contract SimpleDrop is ERC721, LazyMintERC721, ContractMetadata, PermissionsEnumerable, Royalty, Multicall {
+contract SimpleDrop is ERC721, LazyMintERC721, Drop, ContractMetadata, PermissionsEnumerable, Royalty, Multicall {
     constructor() ERC721("MyToken", "MTK") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
@@ -16,6 +17,22 @@ contract SimpleDrop is ERC721, LazyMintERC721, ContractMetadata, PermissionsEnum
     function tokenURI(uint256 _tokenId) public view override(ERC721, LazyMintERC721) returns (string memory) {
         return LazyMintERC721.tokenURI(_tokenId);
     }
+
+    function collectPriceOnClaim(
+        uint256 _quantityToClaim,
+        address _currency,
+        uint256 _pricePerToken
+    ) internal override {
+
+    }
+
+    /// @dev Transfers the NFTs being claimed.
+    function transferTokensOnClaim(address _to, uint256 _quantityBeingClaimed)
+        internal
+        override
+        returns (uint256 startTokenId) {
+            return 0;
+        }
 
      function _canLazyMint() internal view override returns (bool) {
         return hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -26,6 +43,10 @@ contract SimpleDrop is ERC721, LazyMintERC721, ContractMetadata, PermissionsEnum
     }
 
     function _canSetRoyaltyInfo() internal view override returns (bool) {
+       return hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function _canSetClaimConditions() internal view override returns (bool) {
        return hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 }
