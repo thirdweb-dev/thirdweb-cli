d7e50fae1e0e23028cfdd7af8a40f43c028489a8
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-24 07:24:47 +1000

    Add erc1155 base contracts (#66)

--- src/constants/base-contracts.ts
+++ src/constants/base-contracts.ts
@@ -91,6 +91,98 @@ contract Contract is ERC721Drop {
         )
     {}
 }`,
+  ERC1155Base: `// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155Base.sol";
+
+contract Contract is ERC1155Base {
+
+      constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    )
+        ERC1155Base(
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps
+        )
+    {}
+
+}`,
+  ERC1155Signature: `// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155SignatureMint.sol";
+
+contract Contract is ERC1155SignatureMint {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    )
+        ERC1155SignatureMint(
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps,
+            _primarySaleRecipient
+        )
+    {}
+}`,
+  ERC1155LazyMint: `// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+
+contract Contract is ERC1155LazyMint {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    ) ERC1155LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+}`,
+  ERC1155DelayedReveal: `// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155DelayedReveal.sol";
+
+contract Contract is ERC1155DelayedReveal {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    ) ERC1155DelayedReveal(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+}`,
+  ERC1155Drop: `// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155Drop.sol";
+
+contract Contract is ERC1155Drop {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    )
+        ERC1155Drop(
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps,
+            _primarySaleRecipient
+        )
+    {}
+}`,
 };
 
 export default baseContracts;
